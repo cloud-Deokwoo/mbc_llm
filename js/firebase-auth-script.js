8fde1ad6e1ee0d6acf9e760f25d184a40603604b
cloud-Deokwoo <83336969+cloud-Deokwoo@users.noreply.github.com>
2025-07-13 21:38:27 +0900

    Update firebase-auth-script.js

--- js/firebase-auth-script.js
+++ js/firebase-auth-script.js
@@ -3,9 +3,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import { getAuth, signInAnonymously, signInWithCustomToken, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
-import { firebaseConfig } from "./env.js"
-console.log(firebase);
-window.__firebase_config = firebase;
 
 let app;
 let auth;
