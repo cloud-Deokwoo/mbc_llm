aa94840c9cd75f2604f8350f1705dee03b73fab5
cloud-Deokwoo <83336969+cloud-Deokwoo@users.noreply.github.com>
2025-07-13 21:13:36 +0900

    Create env.js

---
+++ js/env.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCzk1hqaEgC8j5JHBTdhlWfw9ToEOX9hrY",
+  authDomain: "mbc-llm.firebaseapp.com",
+  projectId: "mbc-llm",
+  storageBucket: "mbc-llm.firebasestorage.app",
+  messagingSenderId: "622242010374",
+  appId: "1:622242010374:web:2e54040e8ec6bc0562fa87",
+  measurementId: "G-SCBGWN2DMY"
+};
